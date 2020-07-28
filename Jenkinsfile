7360c8a108fde77dc861e3ed7c890b5ccb775efc
davidqatraining <david.j.mccartney@gmail.com>
2020-07-28 10:30:48 +0000

    adding more  scripts .

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,13 +8,9 @@ pipeline {
 
             steps{
 
-                sh 'docker build -t davidqatraining/service_1 ./service_1'
-                sh 'docker build -t davidqatraining/service_2 ./service_2'
-                sh 'docker build -t davidqatraining/service_3 ./service_3'
-                sh 'docker build -t davidqatraining/service_4 ./service_4'
                 sh 'chmod +x ./scripts/*.sh'
                 sh './scripts/before_installation.sh'
-                
+
 
             }
 
--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # if service 1 image does not exsist locally build image, then same for 2,3,4
-if [[ "$(docker images - q davidqatraining/service_1:latest 2> /dev/null)" == "" ]]
-    docker build -t davidqatraining/service_1 ../service_1 #S
+if [[ "$(docker images - q davidqatraining/service_1:latest 2> /dev/null)" == "" ]]; then
+    docker build -t davidqatraining/service_1 ./Service_1 #S
 fi
 
-if [[ "$(docker images - q davidqatraining/service_1:latest 2> /dev/null)" == "" ]]
-    docker build -t davidqatraining/service_2 ../service_2 #S
+if [[ "$(docker images - q davidqatraining/service_1:latest 2> /dev/null)" == "" ]]; then
+    docker build -t davidqatraining/service_2 ./Service_2 #S
 fi
 
-if [[ "$(docker images - q davidqatraining/service_1:latest 2> /dev/null)" == "" ]]
-    docker build -t davidqatraining/service_3 ../service_3 #S
+if [[ "$(docker images - q davidqatraining/service_1:latest 2> /dev/null)" == "" ]]; then
+    docker build -t davidqatraining/service_3 ./Service_3 #S
 fi
 
-if [[ "$(docker images - q davidqatraining/service_1:latest 2> /dev/null)" == "" ]]
-    docker build -t davidqatraining/service_3 ../service_4 #S
+if [[ "$(docker images - q davidqatraining/service_1:latest 2> /dev/null)" == "" ]]; then
+    docker build -t davidqatraining/service_3 ./Service_4 #S
 fi
